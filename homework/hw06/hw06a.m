adb59008609bf3d0e34cd717f9743a29d3685e08
userw <sgoodman@aiddata.org>
2018-04-15 20:09:48 -0400

    Work on hw6

--- homework/hw06/hw06a.m
+++ homework/hw06/hw06a.m
@@ -1,5 +1,9 @@
 % hw06
 
+clear;
+close all;
+
+% continuous time system defined by ABCD
 
 A = [-0.0558 -0.9968  0.0802  0.0415
       0.5980 -0.1150 -0.0318  0
@@ -17,13 +21,29 @@ C = [0 1 0 0
 D = [0 0
      0 0];
 
+sysc = ss(A ,B, C, D);
+
+% define time step
 Ts = 0.1;
-sysc = ss(A ,B, C, D, Ts);
 
-sysd = c2d(sysc);
+% discretize system
+sysd = c2d(sysc, Ts);
 
+% define input vector
+% u = [2 3 5 9; 1 5 7 2;];
+u = [2 3 1; 1 5 1];
 
+% ???
+dlsim(A, B, C, D, u)
 
+% Is the system stable?  
+% How many inputs and outputs does this system have?
+
+% For the discrete-time system, compute all of the system's impulse responses
+figure()
+impulse(sysd)
+
+% Using both convolution and dlsim, demonstrate that you get the same response due to two sets of arbitrary inputs.
 
 
 
